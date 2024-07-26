e92d82437b37295e306ec58f9291d6757378a498
AFREEN-TABASSUM <shaikafreent@gmail.com>
2024-07-26 14:38:57 +0530

    Change

--- src/Boxes.js
+++ src/Boxes.js
@@ -3,14 +3,14 @@ import React from 'react';
 function Boxes({profile}){
   return (
     <div className="profile-box">
-      <img src={profile.profilePic} className="profile-pic" />
+      <img src={profile.profilePic} className="profile-pic"  alt="profile"/>
       <div className="profile-info">
         <h2>{profile.name}</h2>
         <p className="subtitle">{profile.subtitle}</p>
       </div>
       <p>{profile.description}</p>
       <div className="connections">
-        <img src={profile.connectionPic} className="connection-pic" />
+        <img src={profile.connectionPic} className="connection-pic"  alt="profile"/>
         <span>{profile.connections} connections</span>
       </div>
       <hr/>
